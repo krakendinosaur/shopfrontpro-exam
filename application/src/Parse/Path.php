f5417017910f3d29817ee8ef675f6396feaa3e25
Prince Ryan Sy <prince.ryan.sy@gmail.com>
2021-03-02 22:52:00 +0800

    Tree Creation From Parsed Paths (No Leafs Support Yet)

--- application/src/Parse/Path.php
+++ application/src/Parse/Path.php
@@ -6,6 +6,16 @@ use Helpers\Input;
 
 class Path
 {
+    const DEFAULT_INDENTION_SPACES = 4;
+
+    const WEB_SPACE = '&nbsp;';
+
+    const WEB_NEWLINE = '<br />';
+
+    const CLI_SPACE = ' ';
+
+    const CLI_NEWLINE = "\n";
+
     public static function toArray(array $paths)
     {
         try {
@@ -79,5 +89,50 @@ class Path
         }
 
         return $output;
+    }
+
+    public static function createTree(array &$parsedPaths, int $depth, int $leafs, int &$currentCount = 0)
+    {
+        try {
+            if (empty($depth)) {
+                throw new \Exception('Error: Depth does not accept null or zero values.');
+            }
+
+            if (empty($leafs)) {
+                throw new \Exception('Error: Leafs does not accept null or zero values.');
+            }
+
+            $space = self::WEB_SPACE;
+            $newline = self::WEB_NEWLINE;
+
+            if (Input::isCli()) {
+                $space = self::CLI_SPACE;
+                $newline = self::CLI_NEWLINE;
+            }
+
+            $display = '';
+
+            $currentCount++;
+
+            foreach ($parsedPaths as $key => $value) {
+                $spacesCount = ($currentCount - 1) * self::DEFAULT_INDENTION_SPACES;
+
+                if (is_array($value)) {
+                    $display .= str_repeat($space, $spacesCount) . $key . $newline;
+
+                    $depth--;
+
+                    if ($depth > 0) {
+                        $display .= self::createTree($value, $depth, $leafs, $currentCount);
+                    }
+                } else {
+                    $display .= str_repeat($space, $spacesCount) . $value . $newline;
+                }
+            }
+
+            return $display;
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
---
+++ tree.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'init.php';
+
+use Parse\Path;
+
+$paths = array(
+    '/home/user/folder1/folder2/kdh4kdk8.txt',
+    '/home/user/folder1/folder2/565shdhh.txt',
+    '/home/user/folder1/folder2/folder3/nhskkuu4.txt',
+    '/home/user/folder1/iiskjksd.txt',
+    '/home/user/folder1/folder2/folder3/owjekksu.txt'
+);
+
+$parsedPaths = Path::toArray($paths);
+echo Path::createTree($parsedPaths, 4, 3);
