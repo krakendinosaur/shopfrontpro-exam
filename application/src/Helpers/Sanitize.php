2944600ba63911e069d62c9c8b281868c116ffbe
Prince Ryan Sy <prince.ryan.sy@gmail.com>
2021-03-03 13:32:14 +0800

    Added sanitization for string variables

--- application/src/Helpers/Sanitize.php
+++ application/src/Helpers/Sanitize.php
@@ -4,6 +4,18 @@ namespace Helpers;
 
 class Sanitize
 {
+    public static function reduceWhitespaces($str, $trim = false)
+    {
+        $str = preg_replace('/\s+/', ' ', $str);
+        return ($trim === true) ? trim($str) : $str;
+    }
+
+    public static function string($str)
+    {
+        $str = self::reduceWhitespaces($str, true);
+        return filter_var($str, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    }
+
     public static function int($var)
     {
         return filter_var($var, FILTER_SANITIZE_NUMBER_INT);
